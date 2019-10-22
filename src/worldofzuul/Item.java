3c847b6a6ea3caf21540b3750dd57c152cdb466c
nikkerlas <nikves7107@gmail.com>
2019-10-22 11:41:41 +0200

    Update Item.java

--- src/worldofzuul/Item.java
+++ src/worldofzuul/Item.java
@@ -3,16 +3,26 @@ package worldofzuul;
 public class Item {
     private String name;
     private String description;
-
+/**
+ * constructs an item object using 2 strings as parameters
+ * @param name
+ * @param description 
+ */
     public Item(String name, String description) {
         this.name = name;
         this.description = description;
     }
-    
+    /**
+     * Retunere name
+     * @return name
+     */
     public String getName() {
         return name;
     }
-
+/**
+ * Retunere Description
+ * @return description 
+ */
     public String getDescription() {
         return description;
     }
