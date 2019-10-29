9b164729a5e888f83acedf5e6cf4fc556b971ad0
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-10-29 13:54:40 +0100

    Removed comments

--- src/worldofzuul/PointOfInterest.java
+++ src/worldofzuul/PointOfInterest.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package worldofzuul;
 
-/**
- *
- * @author nicol
- * 
- * 
- */
 public class PointOfInterest {
     private String name;
     private String description;
@@ -42,9 +31,4 @@ public class PointOfInterest {
     public String getDescription() {
         return description;
     }
-    
-        
-    
-    
-    
 }
