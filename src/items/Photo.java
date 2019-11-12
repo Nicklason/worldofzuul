33f703427f1df4aeb5ea796ccac89e58e0393a80
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-11-12 11:43:32 +0100

    Fixed camera and added billboard poi

--- src/items/Photo.java
+++ src/items/Photo.java
@@ -2,30 +2,25 @@ package items;
 
 import worldofzuul.Game;
 import pointsofinterest.PointOfInterest;
-import items.Photo;
 
-public class Camera extends Item {
-    public Camera (Game game) {
-        super("camera", "A digital camera", game);
+public class Photo extends Item {
+    public Photo (Game game) {
+        super("photo", "A photo of the secret behind the production of cola", game);
     }
 
     @Override
     public void use () {
         PointOfInterest pointOfInterest = game.getCurrentPointOfInterest();
 
-        // If the poi is already fixed, then don't allow the item to be used (it would keep adding " (with photo)" to the name)
-        if (!game.getCurrentRoom().getName().equals("factory") || !pointOfInterest.getName().equals("map") || pointOfInterest.isFixed()) {
+        if (!game.getCurrentRoom().getName().equals("street") || !pointOfInterest.getName().equals("billboard")) {
             System.out.println("Can't use " + this.getName() + " here");
             return;
         }
-        
+
         pointOfInterest.setFixed();
-        
+
         System.out.println("Using " + this.getName() + " at " + pointOfInterest.getName());
         System.out.println(pointOfInterest.getLongDescription());
-        System.out.println("A photo has been added to your inventory");
-
         this.game.inventory.remove(this);
-        this.game.inventory.add(new Photo(game));
     }
 }
---
+++ src/pointsofinterest/Billboard.java
@@ -0,0 +1,7 @@
+package pointsofinterest;
+
+public class Billboard extends PointOfInterest {
+    public Billboard () {
+        super("billboard", "A billboard, can be used to show a photo", "Your photo is now being viewed by many people, hopefully they change their minds now");
+    }
+}
