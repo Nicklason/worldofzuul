efc3ca053eabcd872f628ddfa2973803522f4451
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-11-12 13:14:08 +0100

    Remove unused import

--- src/pointsofinterest/Boat.java
+++ src/pointsofinterest/Boat.java
@@ -1,9 +1,7 @@
 package pointsofinterest;
 
-import worldofzuul.Game;
-
 public class Boat extends PointOfInterest {
-    public Boat (Game game) {
+    public Boat () {
         super("boat", "You see a boat in the sea. The boats engine is leaking into the water and the water surrounding the boat is contaminated", "you used the boots to get out and fix the leaking boat engine.");
     }
 }
