00c304cff14a558aa9a35062e7ec10c9051ec202
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-11-12 13:12:31 +0100

    Resolve issue for pr

--- src/items/Pumphandle.java
+++ src/items/Pumphandle.java
@@ -2,7 +2,6 @@ package items;
 
 import worldofzuul.Game;
 import pointsofinterest.PointOfInterest;
-import items.Metalpatch;
 
 public class Pumphandle extends Item {
 
@@ -13,14 +12,14 @@ public class Pumphandle extends Item {
     @Override
     public void use() {
         PointOfInterest pointOfInterest = game.getCurrentPointOfInterest();
-        PointOfInterest pointOfInterestBoy = game.getCurrentRoom().getPointOfInterest("boy");
         if (!game.getCurrentRoom().getName().equals("suburbs") || !pointOfInterest.getName().equals("waterpump")) {
             System.out.println("Can't use " + this.getName() + " here");
             return;
         }
 
         pointOfInterest.setFixed();
-        pointOfInterestBoy.setDescription("Thank you for fixing the pump, I will never play with water again!");
+
+        game.getCurrentRoom().getPointOfInterest("boy").setDescription("Thank you for fixing the pump, I will never play with water again!");
                 
         System.out.println("Using " + this.getName() + " at " + pointOfInterest.getName());
         System.out.println(pointOfInterest.getLongDescription());
