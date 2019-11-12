5b309f6ab155a4d9adbb38d31fa3604f19a637e2
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-11-12 13:13:00 +0100

    Remove unused import

--- src/pointsofinterest/Boy.java
+++ src/pointsofinterest/Boy.java
@@ -1,15 +1,11 @@
 package pointsofinterest;
 
-import items.Metalpatch;
 import items.Pumphandle;
 import worldofzuul.Game;
-import pointsofinterest.Waterpump;
 
 public class Boy extends PointOfInterest {
     public Boy (Game game) {
         super("boy", "Oh no, I have been playing with the waterpump and the handle came off! My mom's going to kill me!");
         this.inventory.add(new Pumphandle(game));
-    }  
-  }
-
-
+    }
+}
