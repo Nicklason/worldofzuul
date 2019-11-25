31d1a59c1f9a6b0769741b2d9033d3aaca7e0a7e
nico8034 <56026876+nico8034@users.noreply.github.com>
2019-11-25 21:42:35 +0100

    stylesheep added, styles added, markers added with toggle group

--- src/main/java/com/mycompany/worldofzuul/App.java
+++ src/main/java/com/mycompany/worldofzuul/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("rooms/lobby"));
+        scene.getStylesheets().add("style.css");
         stage.setScene(scene);
         stage.show();
     }
