82434c7344ab454ac11b39a0a8f34e949d7fb689
nico8034 <56026876+nico8034@users.noreply.github.com>
2019-12-07 14:23:29 +0100

    changed FXML default back to menu

--- src/main/java/com/mycompany/worldofzuul/App.java
+++ src/main/java/com/mycompany/worldofzuul/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("rooms/lake"));
+        scene = new Scene(loadFXML("menu/menu"));
         scene.getStylesheets().add("style.css");
         stage.setScene(scene);
         stage.show();
